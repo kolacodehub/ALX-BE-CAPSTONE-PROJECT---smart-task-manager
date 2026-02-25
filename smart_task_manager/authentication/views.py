c96a398486c3cbd6a8c8575da4f59d91b12efc53
Tiamiyu Abdul-salam <128032488+kolacodehub@users.noreply.github.com>
2026-02-25 21:59:09 +0100

    Added User Authentication using JWT token

--- smart_task_manager/authentication/views.py
+++ smart_task_manager/authentication/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from .models import User
+from .serializers import RegisterSerializer
 
-# Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,) 
+    serializer_class = RegisterSerializer
--- smart_task_manager/config/settings.py
+++ smart_task_manager/config/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,3 +126,16 @@ STATIC_URL = "static/"
 
 # Additional setting
 AUTH_USER_MODEL = "authentication.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
+
+# SimpleJWT Configuration
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "AUTH_HEADER_TYPES": ("Bearer",),
+}

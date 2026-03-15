0eb52f36ddeaf1ed19885d3bcd2511ce1fae9ef9
Tiamiyu Abdul-salam <128032488+kolacodehub@users.noreply.github.com>
2026-03-15 07:56:56 +0100

    added the vercel frontend link

--- smart_task_manager/config/settings.py
+++ smart_task_manager/config/settings.py
@@ -42,7 +42,7 @@ ALLOWED_HOSTS = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://smartask.vercel.app",  
+    "https://smartask-murex.vercel.app",
 ]
 
 # Application definition

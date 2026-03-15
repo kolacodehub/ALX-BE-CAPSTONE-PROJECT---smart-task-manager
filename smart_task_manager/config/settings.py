d29904b638540736e57e638d0835c573882b8eb6
Tiamiyu Abdul-salam <128032488+kolacodehub@users.noreply.github.com>
2026-03-15 08:15:13 +0100

    added local testing

--- smart_task_manager/config/settings.py
+++ smart_task_manager/config/settings.py
@@ -43,6 +43,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://smartask-murex.vercel.app",
+    "http://localhost:5173",
 ]
 
 # Application definition
